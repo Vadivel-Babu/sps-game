5afc48680d48ba73477bc8c105dcb3fc83c31114
Vadivel-Babu <vadivelbabu1996@gmail.com>
2025-08-15 06:22:39 +0530

    variable initiated

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("hi");
+"use strict";
+window.onload(() => {
+  let computerScore = document.querySelector(".score.computer");
+  let humanScore = document.querySelector(".score.human");
+  let choiceBtn = document.querySelectorAll(".choice");
+  let resultSection = document.querySelector(".result");
+  let winningSection = document.querySelector(".winning");
+  let humanTotalScore;
+  let computerTotalScore;
+  const choices = ["rock", "paper", "scissor"];
+});
