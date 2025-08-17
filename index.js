992928adf4ae288e88cc10729b78df988001e317
Vadivel-Babu <vadivelbabu1996@gmail.com>
2025-08-17 22:30:58 +0530

    handler fuction created

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 "use strict";
-window.onload(() => {
+window.onload = function () {
   let computerScore = document.querySelector(".score.computer");
+  let scoreBoard = document.querySelector(".scoreboard");
   let humanScore = document.querySelector(".score.human");
-  let choiceBtn = document.querySelectorAll(".choice");
+  const choicesBtn = document.querySelectorAll(".choice__btn");
+  let gameboardSection = document.querySelector(".gameboard");
+  let rulesModal = document.querySelector(".rules__modal");
   let resultSection = document.querySelector(".result");
   let winningSection = document.querySelector(".winning");
-  let humanTotalScore;
-  let computerTotalScore;
+  let playAgainBtn = document.querySelectorAll(".btn__play_again");
+  const nextBtn = document.querySelector(".next-btn");
+  const rulesBtn = document.querySelector(".rules-btn");
+  const modalCloseBtn = document.querySelector(".btn__close--modal");
+  let humanTotalScore = 0;
+  let computerTotalScore = 0;
   const choices = ["rock", "paper", "scissor"];
-});
+
+  //selecting the choice
+  choicesBtn.forEach((choice) => {
+    choice.addEventListener("click", (e) => {
+      const value = e.target.getAttribute("data-value");
+      const computerChoice = Math.floor(Math.random() * 2) + 1;
+      console.log(value, choices[computerChoice]);
+      if (value === choices[computerChoice]) {
+        console.log("match tie");
+        nextBtn.style.display = "none";
+        gameboardSection.style.display = "none";
+        resultSection.style.display = "flex";
+      } else if (
+        (value === "rock" && choices[computerChoice] === "paper") ||
+        (value === "paper" && choices[computerChoice] === "scissor") ||
+        (value === "scissor" && choices[computerChoice] === "rock")
+      ) {
+        computerTotalScore++;
+        computerScore.textContent = computerTotalScore;
+        nextBtn.style.display = "none";
+        gameboardSection.style.display = "none";
+        resultSection.style.display = "flex";
+      } else if (
+        (value === "paper" && choices[computerChoice] === "rock") ||
+        (value === "scissor" && choices[computerChoice] === "paper") ||
+        (value === "rock" && choices[computerChoice] === "scissor")
+      ) {
+        humanTotalScore++;
+        humanScore.textContent = humanTotalScore;
+        nextBtn.style.display = "inline";
+        gameboardSection.style.display = "none";
+        resultSection.style.display = "flex";
+      }
+    });
+  });
+
+  //play again handler
+  playAgainBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      winningSection.style.display = "none";
+      resultSection.style.display = "none";
+      gameboardSection.style.display = "grid";
+      scoreBoard.style.display = "flex";
+      nextBtn.style.display = "none";
+    });
+  });
+
+  //modal open button handler
+  rulesBtn.addEventListener("click", () => {
+    rulesModal.style.display = "block";
+  });
+
+  //modal close button handler
+  modalCloseBtn.addEventListener("click", () => {
+    rulesModal.style.display = "none";
+  });
+
+  //next button handler
+  nextBtn.addEventListener("click", () => {
+    scoreBoard.style.display = "none";
+    resultSection.style.display = "none";
+    gameboardSection.style.display = "none";
+    winningSection.style.display = "block";
+  });
+};
